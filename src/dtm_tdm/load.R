bb3de2160bfb69f1dbd74b94a758ae9b9af9d067
mdcRed <my.coyne@gmail.com>
2015-11-21 01:18:26 -0500

    final coding

--- src/dtm_tdm/load.R
+++ src/dtm_tdm/load.R
@@ -4,6 +4,6 @@
 ## is combined of business and review text for select 
 ##    states of interest, which are: AZ, NV, NC
 ## ===================================================
-load("~/R/basicTm/capstone/processData/AZ_df.Rdata")
-load("~/R/basicTm/capstone/processData/NV_df.Rdata")
-load("~/R/basicTm/capstone/processData/NC_df.Rdata")
+load('C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/AZ_df.corpus.Rdata')
+load('C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/AZ_df.corpus.Rdata')
+load('C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/AZ_df.corpus.Rdata')
