415a11b58f4b74662bce3834e4b319ac42209b72
mdcRed <my.coyne@gmail.com>
2015-11-21 01:19:12 -0500

    final coding

--- src/getAndPrepData/getDataFromDb.R
+++ src/getAndPrepData/getDataFromDb.R
@@ -15,14 +15,21 @@ options(stringAsFactors=FALSE)
 connHandle <- odbcConnect("p-sql-w-ehr01.athena_dev", uid="ehruser", pwd="ehruser123");
 
 sqlString <- paste0(
-  "select b.state, COUNT(*) number_of_reviews		   "
+  "select b.state, COUNT(distinct b.id) number_of_reviews		   "
   ," from athena_dev.dbo.temp_bdf  b					   "
   ," where 1 = 1										   "
   ," and   (lower(categories) like '%doctor%'			   "
   ,"       or    lower(categories) like '%physician%'	   "
   ,"       or    lower(categories) like '%health%'	   "
   ,"       or    LOWER(categories) like '%medic%')	   "
-  ," and   (LOWER(categories) not like '%market%')	   "
+  ," and   (      LOWER(categories) not like '%market%'		 "
+  ,"        and   LOWER(categories) not like '%Massage%'  	 "
+  ,"        and   LOWER(categories) not like '%Gyms%' 		 "
+  ,"        and   LOWER(categories) not like '%pilate%'		 "
+  ,"        and   LOWER(categories) not like '%phopping%'		 "
+  ,"        and   LOWER(categories) not like '%ppa%'			 "
+  ,"        and   LOWER(categories) not like '%peight Loss%'	 "
+  ,"        and   LOWER(categories) not like '%acupuncture%' )	 "
   ," group by b.state									   "
   ," order by COUNT(*)								   "
 )
---
+++ src/getAndPrepData/save.R
@@ -0,0 +1,6 @@
+### Save AZ_df files
+
+
+save(AZ_df,file='C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/AZ_df.Rdata')
+save(NV_df,file='C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/NV_df.Rdata')
+save(NC_df,file='C:/Users/mcoyne/Documents/R/basicTm/capstone/processData/NC_df.Rdata')
